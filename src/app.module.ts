1df8ce773807a0857486f8f339a91474b7876bdd
Nagi2880 <alexanderamaral17@gmail.com>
2023-11-05 05:18:17 -0300

    User module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost/nest'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
